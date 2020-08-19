7c2de1554a53176f7e61f6ffb8132d3ddcebf833
Deadline <seth.parson@cityxen.net>
2020-08-18 20:36:23 -0400

    update Clicky

---
+++ Clicky (Commodore 64)/Clicky_Decorations_Data.asm
@@ -0,0 +1,44 @@
+//////////////////////////////////////////////////
+// Decorations Tables
+
+color_table_offset:
+.byte 0
+characters_color_table:
+.byte GREEN
+.byte RED
+.byte BLUE
+.byte YELLOW
+.byte PURPLE
+.byte GRAY
+.byte $ff
+sprite_multicolor_0_table:
+.byte GREEN
+.byte RED
+.byte BLUE
+.byte YELLOW
+.byte PURPLE
+.byte GRAY
+sprite_multicolor_1_table:
+.byte LIGHT_GREEN
+.byte LIGHT_RED
+.byte LIGHT_BLUE
+.byte ORANGE
+.byte CYAN
+.byte LIGHT_GRAY
+
+halloween_scar_status:
+.byte 0
+halloween_scar:
+.byte $20,$20,$20,$4d,$4e,$20,$20,$20,$20
+.byte $20,$20,$4d,$4e,$4d,$20,$20,$20,$20
+.byte $20,$4d,$4e,$4d,$20,$20,$20,$20,$20
+.byte $20,$4e,$4d,$20,$20,$20,$20,$20,$20
+halloween_scar_clear:
+.byte $20,$20,$20,$20,$20,$20,$20,$20,$20
+.byte $20,$20,$20,$20,$20,$20,$20,$20,$20
+.byte $20,$20,$20,$20,$20,$20,$20,$20,$20
+.byte $20,$20,$20,$20,$20,$20,$20,$20,$20
+
+valentine_cry_status:
+.byte 0,0,0,0,0
+
