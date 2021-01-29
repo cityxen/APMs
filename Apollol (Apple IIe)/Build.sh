029d838e4351bceca56dd602f603cc46b035c581
Seth Parson aka Deadline <seth.parson.71@gmail.com>
2021-01-29 05:13:02 -0500

    fix linux build script for Apollol

--- Apollol (Apple IIe)/Build.sh
+++ Apollol (Apple IIe)/Build.sh
@@ -1,14 +1,13 @@
-vasm6502 Apollol.asm -c02 -chklabels -nocase -Fvasm=1 -DBuildAP2=1 -Fbin -o "APOLLOL.BIN" # -L listing.txt
+vasm6502 Apollol.asm -c02 -chklabels -nocase -Fvasm=1 -DBuildAP2=1 -Fbin -o "APOLLOL" # -L listing.txt
 retval=$?
 echo $retval
 if [ $retval -eq 0 ]
 then
     rm APOLLOL.DSK
     cp BLANK.DSK APOLLOL.DSK
-    a2in b.0c00 APOLLOL.DSK APOLLOL.BIN APOLLOL.BIN
+    a2in b.0c00 APOLLOL.DSK APOLLOL APOLLOL
     a2ls APOLLOL.DSK
     AppleWin -d1 APOLLOL.DSK
-
 else
     echo "Error in asm file"
 fi
---
+++ Apollol (Apple IIe)/build_linux.sh
@@ -0,0 +1,12 @@
+echo "Building Apollol"
+vasm6502 Apollol.asm -c02 -chklabels -nocase -Fvasm=1 -DBuildAP2=1 -Fbin -o "APOLLOL" # -L listing.txt
+if [ $? -eq 0 ]; then
+    rm APOLLOL.DSK
+    cp BLANK.DSK APOLLOL.DSK
+    a2in b.0c00 APOLLOL.DSK APOLLOL APOLLOL
+    a2ls APOLLOL.DSK
+    applewin APOLLOL.DSK
+else
+    echo "Error in asm file"
+fi
+
